fbf33496fb7698692fdf5760e8cf2e540e16792b
Kyle Roman <kyle.roman@sonyatv.com>
2021-01-25 21:33:42 -0600

    Deliverable 2 submission

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Deliverable 1
 SELECT e.emp_no, 
 	e.first_name, 
 	e.last_name,
@@ -25,3 +26,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(emp_no) DESC
+
+
+-- Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date, 
+	 ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND de.to_date = '9999-01-01'
+ORDER BY e.emp_no, ti.to_date DESC;
